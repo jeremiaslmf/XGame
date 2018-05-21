8f7ecbac74d632fb32f2ff6f1fbdfb4d444e000b
Jeremias Lima <jeremiaslmf@gmail.com>
2018-05-21 11:29:59 -0300

    Implementação Camada de Infraestrutura
    - ORM: Entity Framework

---
+++ XGame.Infra/Persistence/Map/MapJogador.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using XGame.Domain.Entities;
+
+namespace XGame.Infra.Persistence.Map
+{
+    public class MapJogador : EntityTypeConfiguration<Jogador>
+    {
+        public MapJogador()
+        {
+            ToTable("Jogador");
+
+            Property(x => x.Nome.PrimeiroNome)
+                .HasColumnName("PrimeiroNome")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            Property(x => x.Nome.UltimoNome)
+                .HasColumnName("UltimoNome")
+                .HasMaxLength(50)
+                .IsRequired();
+                
+            Property(x => x.Email.Endereco)
+                .HasMaxLength(200)
+                .IsRequired()
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("UK_JOGADOR_EMAIL") { IsUnique = true })).HasColumnName("Email");
+
+            Property(x => x.Senha).HasMaxLength(32).IsRequired();
+
+            Property(x => x.Status).IsRequired();
+
+        }
+    }
+}
---
+++ XGame.Infra/Persistence/XGameContext.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using XGame.Domain.Entities;
+
+namespace XGame.Infra.Persistence
+{
+    public class XGameContext : DbContext
+    {
+        public XGameContext() : base("xGameConnectionString")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public IDbSet<Jogador> Jogadores { get; set; }
+        public IDbSet<Plataforma> Plataformas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Seta o Schema default
+            // modelBuilder.HasDefaultSchema("Apoio");
+
+            // Remove a pluralização dos nomes das tabelas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // Remove exclusão em cascata
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            // Setar para usar varchar invés de nvarchar
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            // Seta o tamanho padrão para determinado campo caso não seja informado o tm em sua criação
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            // Mapeia as entidades
+            // modelBuilder.Configurations.Add(new JogadorMap());
+            // modelBuilder.Configurations.Add(new PlataformaMap());
+
+            #region Adiciona entidades mapeadas Automaticamente via Assembly
+            /// <summary>
+            /// Isso facilita o mapeamento das entidades.
+            /// O trecho comentado acima, não é necessário ser feito pois o Assembly faz isso sem o Add para cada mapeamento
+            /// </summary>
+
+            modelBuilder.Configurations.AddFromAssembly(typeof(XGameContext).Assembly);
+
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
