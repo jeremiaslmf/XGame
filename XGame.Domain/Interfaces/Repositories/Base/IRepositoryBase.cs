b899f90dc23f73f11893d40567bf9bae33805d3d
Jeremias Lima <jeremiaslmf@gmail.com>
2018-05-21 17:54:58 -0300

    1 - Implementação do GenericRepository

---
+++ XGame.Domain/Interfaces/Repositories/Base/IRepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace XGame.Domain.Interfaces.Repositories.Base
+{
+    public interface IRepositoryBase<TEntidade, TId>
+        where TEntidade : class
+        where TId : struct
+    {
+        IQueryable<TEntidade> ListarPor(Expression<Func<TEntidade, bool>> where, params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        IQueryable<TEntidade> ListarEOrdenarPor<TKey>(Expression<Func<TEntidade, bool>> where, Expression<Func<TEntidade, Tkey>>);
+
+        TEntidade ObeterPor(Func<TEntidade, bool> where, params Expression<Func<TEntidade, object>>[]);
+
+        bool Existe(Func<TEntidade, bool> where);
+
+        
+    }
+}
